89f5e5b5c6a79cff90af98bed030f82e50cf3319
DeifMohamed2 <1+3=42+2=4q>
2026-02-27 16:15:00 +0200

    Create test FTP supplier

--- scripts/enableFtpTestUser.js
+++ scripts/enableFtpTestUser.js
@@ -2,33 +2,44 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 require("dotenv").config();
 
-async function enableFTP() {
+async function createAndEnableFTP() {
   await mongoose.connect(process.env.MONGODB_URI);
   const Supplier = require("../models/Supplier");
   
-  const supplier = await Supplier.findOne({}).select("companyName email");
+  // Check if test supplier exists
+  let supplier = await Supplier.findOne({ email: "testftp@partsform.com" });
+  
   if (!supplier) {
-    console.log("No suppliers found");
-    await mongoose.disconnect();
-    return;
-  }
-  
-  const ftpPassword = "TestFTP123";
-  const hashedPassword = await bcrypt.hash(ftpPassword, 10);
-  
-  await Supplier.updateOne(
-    { _id: supplier._id },
-    { 
+    // Create a test supplier
+    const passwordHash = await bcrypt.hash("Test123456", 10);
+    supplier = await Supplier.create({
+      companyName: "Test FTP Supplier",
+      email: "testftp@partsform.com",
+      password: passwordHash,
+      phone: "+1234567890",
+      isApproved: true,
       ftpEnabled: true,
       ftpUsername: "testftp",
-      ftpPassword: hashedPassword
-    }
-  );
+      ftpPassword: await bcrypt.hash("TestFTP123", 10)
+    });
+    console.log("Created new test supplier");
+  } else {
+    // Update existing supplier
+    await Supplier.updateOne(
+      { _id: supplier._id },
+      { 
+        ftpEnabled: true,
+        ftpUsername: "testftp",
+        ftpPassword: await bcrypt.hash("TestFTP123", 10)
+      }
+    );
+    console.log("Updated existing supplier");
+  }
   
-  console.log("FTP enabled for supplier:", supplier.companyName);
+  console.log("FTP enabled for:", supplier.companyName);
   console.log("FTP Username: testftp");
   console.log("FTP Password: TestFTP123");
   
   await mongoose.disconnect();
 }
-enableFTP();
+createAndEnableFTP();
